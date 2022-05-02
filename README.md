5bd330b6e9325792c9e6b209cdf6de65a06c96bc
fstaryk <fstaryk@student.42wolfsburg.de>
2022-05-02 13:59:25 +0200

    update for instatnt use

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 For now only script works, and it's already painful enough. In future I plan to write C programm that will do it automaticaly
 and go in daemon as well, so its imposible to turn off
 
+To use 
+zsh rick_roll.sh
+
+Skip all options for infinite rick roll
+
 Have fun everybody))))))))))))
+
--- rick_roll.sh
+++ rick_roll.sh
@@ -1,9 +1,22 @@
+echo "You can always skip with ENTER to get default joke"
 echo "What is the desired link?"
 read link
+if [ -z "$link" ]
+then
+	link="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+fi
 echo "How many times repeat it?(inf for infinity)"
 read count
+if [ -z "$count" ]
+then
+	count=inf
+fi
 echo "What would be a reapiting frase?(leave empty for nothing)"
 read frase
+if [ -z "$frase" ]
+then
+	frase="get rick rolled"
+fi
 
 if ($count -eq inf)
 then
